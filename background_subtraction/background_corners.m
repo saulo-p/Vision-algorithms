beefa095e935b238d891c4db489a7bb7441359c9
Saulo Pereira <saulo.sobrinho@gprt.ufpe.br>
2017-11-21 18:00:21 -0300

    Background Subtraction: [WIP] Abordagem por linhas

--- background_subtraction/background_corners.m
+++ background_subtraction/background_corners.m
@@ -6,10 +6,10 @@
 clear all;
 close all;
 
-im = im2double(rgb2gray(imread('./anteparo_pat/img_2.png')));
+%% Setup
+% Input data
+im = im2double(rgb2gray(imread('./anteparo_pat/img_3.png')));
 im_sz = size(im);
-
-%% Corner approach
 
 % Parameters
 gaussian_size = 7;
@@ -17,43 +17,40 @@ gaussian_sigma = 1.0;
 block_size = 5;
 tol = 0.25;
 
+%% Main code
 
-% Pre processing
+%>Pre processing
 L = fspecial('gaussian', gaussian_size, gaussian_sigma);
 iml = imfilter(im, L);
 
-% Corner based filtering
+%>Corner based filtering
 C = [-1*ones(block_size) ones(block_size);
      ones(block_size) -1*ones(block_size)];
 iml = imrotate(iml, 45);
 imf = imfilter(iml, C);
 imf = imrotate(imf, -45);
- %crop back to original size
+% crop back to original size
 rot_sz = size(imf);
 imc = imcrop(imf, [(rot_sz(2)-im_sz(2))/2 (rot_sz(1)-im_sz(1))/2 ... 
                     im_sz(2)-1 im_sz(1)-1]);
-% imshow(imc);
-% figure;
-% imshow(im);
 imf = NormalizeImage(imc);
 
+%>Binarization 
 imb = imf > 1 - tol | imf < tol;
-imshow(imb);
+% imshow(imb);
 
+% TEST: Scatter regions over image
 [cfx, cfy] = find(imb);
 cf = [cfy cfx];
+imshow(im); hold on; scatter(cf(:,1), cf(:,2), 'r*');
 
-imshow(im);
-hold on
-scatter(cf(:,1), cf(:,2), 'r*');
+%>Connected regions centroids computation
+cc = bwconncomp(imb);
+centroids = zeros(2, length(cc.PixelIdxList));
+for i = 1:length(cc.PixelIdxList)
+    [cs, rs] = ind2sub(im_sz, cc.PixelIdxList{i});
+    
+    centroids(:,i) = [mean(rs) mean(cs)]';
+end
+scatter(centroids(1,:), centroids(2,:), 'b.');
 
-% %  VERTICAL GAP APPROACH (WIP)
-% % sort on x coordinate
-% [~, idx] = sort(cf(:,1));
-% cf = [cf(idx,1) cf(idx,2)];
-% scatter(cf(:,1), cf(:,2), 'b');
-% 
-% % find vertical gap
-% [gap, gap_idx] = max(cf(2:end,1) - cf(1:end-1,1));
-% plot([cf(gap_idx,1) cf(gap_idx,1)], [0 im_sz(1)], 'g');
-% plot([cf(gap_idx+1,1) cf(gap_idx+1,1)], [0 im_sz(1)], 'g');
--- background_subtraction/background_lines.m
+++ background_subtraction/background_lines.m
@@ -6,27 +6,75 @@
 clear all;
 close all;
 
-im = im2double(rgb2gray(imread('./anteparo_pat/img_2.png')));
+%% Setup
+% Input data
+im = im2double(rgb2gray(imread('./anteparo_pat/img_3.png')));
 im_sz = size(im);
 
-% % STEPS
-% rotate image
-% find horizontal lines
-% find end points of lines
-% connect end points to form polygon
+gaussian_size = 7;
+gaussian_sigma = 0.8;
 
+%% Main code
 
-%% HPF approach (talvez complementar a anterior)
+%>Filters
+L = fspecial('gaussian', gaussian_size, gaussian_sigma);
+H = fspecial('sobel');
 
-% L = fspecial('gaussian', 7, 1);
-% H = fspecial('sobel');
-% 
-% imf = imfilter(im, L);
-% imf = imfilter(imf, H) + imfilter(imf, H') + imfilter(imf, -H) + imfilter(imf, -H');
-% 
-% imshow(imf, []);
-% imb = imbinarize(imf);
-% 
+imf = imfilter(im, L);
+% figure;
+% subplot(2,2,1); imshow(imfilter(imf,H)); subplot(2,2,2); imshow(imfilter(imf,H'));
+% subplot(2,2,3); imshow(imfilter(imf,-H)); subplot(2,2,4); imshow(imfilter(imf,-H'));
+
+% Combines Sobel filters in each direction and performs threshold
+% binarization. TODO: binarize by bw percentage.
+imb = imfilter(imf, H) > 0.4  | imfilter(imf, H') > 0.4 | ...
+      imfilter(imf, -H) > 0.4 | imfilter(imf, -H') > 0.4;
+
+%>Erosions
+len = 7;
+%Line erosions
+steL1 = strel('line',len,45);
+steL2 = strel('line',len,-45);
+imbe = imerode(imb,steL1) | imerode(imb,steL2);
+
+% subplot(1,2,1); imshow(im + 0.5*imb);
+% subplot(1,2,2); imshow(im + 0.5*imbe);
+
+%% Vertical Gap:
+v_ws = sum(imb, 1);
+[~, gap_idx] = find(v_ws < 10);
+
+%>Find vertical gap
+% [gap, gap_idx] = max(v_sums(2:end) - v_sums(1:end-1));
+figure; imshow(im + 0.5*imb); hold on;
+plot([gap_idx(1) gap_idx(1)], [0 im_sz(1)], 'g');
+plot([gap_idx(end) gap_idx(end)], [0 im_sz(1)], 'g');
+
+%% Find frontiers
+%>Separates images
+imbs{1} = imb;
+imbs{1}(:,gap_idx(end-1):end) = 0;
+imbs{2} = imb;
+imbs{2}(:, 1:gap_idx(2)) = 0;
+
+%>Determine frontier pixels
+for i = 1:2
+    front{i} = zeros(1, size(imbs{i},1));
+end
+for j = 1:size(imbs{1},1)
+    front{1}(j) = max(find(imbs{1}(j,:)));
+end
+for j = 1:size(imbs{2},1)
+    front{2}(j) = min(find(imbs{2}(j,:)));
+end
+
+%TEST: 
+for i = 1:2
+    subplot(2,1,i); imshow(imbs{i}'); hold on; scatter(1:length(front{i}),front{i},'.');
+%     subplot(2,1,i); plot(front{i});
+end
+    
+%% OLD
 % [H, tta, rho] = hough(imb, 'Theta', -10:10);
 % % figure; imshow(H, []); axis normal;
 % peaks = houghpeaks(H, 15);
